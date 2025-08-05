97af77c1e5efe610e2a0d20e9fde32bf233c154b
VendorIQ <vendorIQ.mark@gmail.com>
2025-08-05 17:19:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -627,7 +627,7 @@ if (!user) {
         key={idx}
         style={{
 		  maxWidth: 1000,
-          margin: "20px 0 0 0",
+          margin: "2px 0 0 0",
           padding: "0 0 20px",
           fontSize: "0.9rem",
           color: "#383a42", // dark GPT gray
